a356556efd08da115264110797f261d069d97052
EmmaK <emmaculatewkamau@gmail.com>
2022-03-15 15:30:37 +0300

    Add user logic to accept user input

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 //Person constructor
 
 function myContact(fName,lName){
-    this.fName = fName
-    this.lName = lName
+    this.firstName = fName
+    this.lastName = lName
 }
 
 //Adress constructor
 
-function myAdresses(street,city,state){
+function adresses(street,city,state){
     this.street = street
     this.city = city
     this.state = state
 }
+
+//JQuery
+
+$(document).ready(function(){
+    $("form#address-form").submit(function(event){
+        event.preventDefault();
+        var firstName = $("input#fname").val()
+        var lastName = $("input#lname").val()
+        var street = $("input#").val()
+        var town = $("input#").val()
+        var county = $("input#").val()
+
+        var newContact = new myContact(firstName,lastName)
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+        console.log(newContact)
+
+        $("input#fname").val("")
+        $("input#lname").val("")
+    }) 
+})
