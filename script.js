0a820d49004e7d10bb9fb7a62c7911c146e7f79a
EmmaK <emmaculatewkamau@gmail.com>
2022-03-15 16:30:46 +0300

    Add click function to add extra div's for other addresses

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 function myContact(fName,lName){
     this.firstName = fName
     this.lastName = lName
+    this.addresses = []
 }
 
 //Adress constructor
@@ -14,21 +15,46 @@ function adresses(street,city,state){
 }
 
 //JQuery
-
 $(document).ready(function(){
     $("form#address-form").submit(function(event){
+        //Form inputs received and processed
         event.preventDefault();
         var firstName = $("input#fname").val()
         var lastName = $("input#lname").val()
-        var street = $("input#").val()
-        var town = $("input#").val()
-        var county = $("input#").val()
+        var street = $("input#street").val()
+        var town = $("input#town").val()
+        var county = $("input#county").val()
 
         var newContact = new myContact(firstName,lastName)
+        var newAddress = new adresses(street,town,county)
+
+        newContact.addresses.push(newAddress)
+
         $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
         console.log(newContact)
+        console.log(newContact.addresses)
 
         $("input#fname").val("")
         $("input#lname").val("")
     }) 
+    //Add new address input form
+    $("span#add-address-input").click(function(){
+        $("div#new-addresses").append(
+            "<div class='new-addresses'>"+
+                "<div class='form-group'>"+
+                    "<label for='street'>Street</label>"+
+                    "<input type='text' class='form-control' id='street' placeholder='Sonia Lane'>"+
+                "</div>"+
+                "<div class='form-group'>"+
+                    "<label for='town'>Town</label>"+
+                    "<input type='text' class='form-control' id='town' placeholder='Mtwapa'>"+
+                "</div>"+
+                "<div class='form-group'>"+
+                    "<label for='county'>County</label>"+
+                    "<input type='text' class='form-control' id='county' placeholder='Kilifi'>"+
+                "</div>"+
+            "</div>"
+        )
+    })
 })
+
