efa3120ad680e34da02661ef6faf5f8605ef8516
EmmaK <emmaculatewkamau@gmail.com>
2022-03-15 15:06:07 +0300

    Add address and contact constructors

---
+++ script.js
@@ -0,0 +1,14 @@
+//Person constructor
+
+function myContact(fName,lName){
+    this.fName = fName
+    this.lName = lName
+}
+
+//Adress constructor
+
+function myAdresses(street,city,state){
+    this.street = street
+    this.city = city
+    this.state = state
+}
